48cc2fbb8fd8df6f32b0a39a5bb753356364e727
CagatayD <32710235+cagdu@users.noreply.github.com>
2023-11-10 02:49:14 +0300

    update

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+import { Application, Request, Response, NextFunction } from "express";
+import { Pool, PoolConfig, QueryOptions } from "mysql";
+import { Strategy } from "passport-strategy";
+import _bcrypt from "bcrypt";
+
+interface ResponseOptions { id?: string, status?: number, data?: Array<T> | object | string, errors?: Array<T> | string };
+function RequestHandle(req: Request, res: Response, next: NextFunction): any;
+
+class Util {
+    data(path: string, use_FS: boolean): object;
+    random(length: number, size: number, splitter: "-" | string): String;
+    lang(text: string, values: Array<T> | string | 0, language: "tr"): String;
+    log(text: string, values: Array<T> | string | 0, path: string, platform?: "discord" | "mysql" | "web" | 0, type?: "log" | "warn" | "error"): any;
+    response(request: request, options?: ResponseOptions | string | number): any;
+
+    options = require("../data/system/config.json");
+    page_options(page: string): object;
+    render(req: Request, page: string, customJson: object): any;
+}
+
+const src = new class CagatayD extends Util {
+    /** @example .db({ sql: "SELECT 1=?", values:[1] }).then(...) */
+    db(query: string | QueryOptions, values: Array<T>): Promise<{ error: boolean, data: Array<T> }>;
+    crypto = new class Crypto {
+        private module = _bcrypt;
+        hash(text: string): Promise<String>;
+        compare(text: string, hash: string): Promise<boolean>;
+    }
+    Middleware = class Middleware {
+        constructor(...arg: Array<T>): any;
+        /** @example new Middleware(["GET", "POST"]).allowMethod */
+        allowMethod = RequestHandle;
+        /** @example new Middleware(permLevel: Number).permission */
+        permission = RequestHandle;
+    }
+    passport = new class Passport {
+        cagataydcom: (options: object, verify: Function) => Strategy;
+        local: (options: object, verify: Function) => Strategy;
+    }
+    schema = new class Schema {
+        user(arg: object): { id: Number, uuid: String, username: String };
+    }
+    struct = new class Struct {
+        express(app: Application, options?: { passport: boolean }): Application;
+        mysql(config?: PoolConfig): Pool;
+    }
+}
+
+export = src;
---
+++ index.js
@@ -0,0 +1,12 @@
+const lib = _ => require("./lib/" + _);
+const src = new class CagatayD extends require("./lib/util") {
+    crypto = lib("crypto");
+    Middleware = lib("middleware");
+    passport = lib("passport/index");
+    schema = lib("schema");
+    struct = lib("struct");
+
+    db = async (query = { sql: null, values: [] }, values = []) => new Promise((r) => { let a = { error: null, data: null }; this.struct.mysql().getConnection((err, c) => { function fy(x, y) { function cb(_a, _b) { c.release(); return r({ error: _a, data: _b }) }; if (y.length > 0) c.query(x, y, cb); else c.query(x, cb) }; if (err) return r({ ...a, error: err }); if (typeof query === "string") return fy(query, values); else return fy(query.sql, query.values) }) });
+}
+
+module.exports = src;
